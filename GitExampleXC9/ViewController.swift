78571d7320ab2a0163b61cd9b13e2693570e6c9a
Sir <sirgay11@gmail.com>
2018-06-13 08:49:08 -0700

    Initial Commit

---
+++ GitExampleXC9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitExampleXC9
+//
+//  Created by Sir on 6/13/18.
+//  Copyright © 2018 Sir. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
